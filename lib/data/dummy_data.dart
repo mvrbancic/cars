58a6c313293c8888be96e4990551af08cad2920e
Marko <salixbit2021@gmail.com>
2024-12-28 23:27:06 +0100

    Init commit

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+var carCategories = [
+  const Category(
+    id: 'toyota',
+    title: 'Toyota',
+    color: Colors.red,
+  ),
+  const Category(
+    id: 'volkswagen',
+    title: 'Volkswagen',
+    color: Colors.blue,
+  ),
+  const Category(
+    id: 'bmw',
+    title: 'BMW',
+    color: Colors.grey,
+  ),
+  const Category(
+    id: 'mercedes',
+    title: 'Mercedes-Benz',
+    color: Colors.black,
+  ),
+  const Category(
+    id: 'honda',
+    title: 'Honda',
+    color: Colors.indigo,
+  ),
+  Category(
+    id: 'ford',
+    title: 'Ford',
+    color: Colors.blue[900]!,
+  ),
+  Category(
+    id: 'tesla',
+    title: 'Tesla',
+    color: Colors.red[700]!,
+  ),
+  Category(
+    id: 'hyundai',
+    title: 'Hyundai',
+    color: Colors.blue[300]!,
+  ),
+];
